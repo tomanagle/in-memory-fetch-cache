60eda4b442c97a61f4dacaf32d23cc8162227159
Tom Nagle <tomanagle@gmail.com>
2020-07-14 12:10:05 +1000

    first commit

---
+++ src/in-memory-fetch-cache.ts
@@ -0,0 +1,71 @@
+export class Cache<T, K> {
+  cache: { [key: string]: T & { fetchDate: Date } }
+  millisecondsToLive: number
+  fetchFunction: Function
+  fetchDate: Date
+  hashKey: string
+
+  constructor({
+    fetchFunction,
+    minutesToLive = 10,
+    hashKey
+  }: {
+    fetchFunction: Function
+    minutesToLive?: number
+    hashKey: string
+  }) {
+    this.millisecondsToLive = minutesToLive * 60 * 1000
+    this.fetchFunction = fetchFunction
+    this.cache = {}
+    this.getData = this.getData.bind(this)
+    this.isCacheExpired = this.isCacheExpired.bind(this)
+    this.fetchDate = new Date(0)
+    this.hashKey = hashKey
+  }
+
+  isCacheExpired(props: K) {
+    // @ts-ignore
+    const item = this.cache[props[this.hashKey]]
+    if (item) {
+      const fetchDate = item.fetchDate
+      return fetchDate.getTime() + this.millisecondsToLive < new Date().getTime()
+    }
+    return true
+  }
+  getData(props: K) {
+    // @ts-ignore
+    const item = this.cache[props[this.hashKey]]
+
+    // Fetch a fresh copy and add it to the cache
+    if (!item || !Object.keys(this.cache).length || this.isCacheExpired(props)) {
+      return this.fetchFunction(props).then((data: any) => {
+        if (data) {
+          // @ts-ignore
+          this.cache[props[this.hashKey]] = { ...data, fetchDate: new Date() }
+
+          return data
+        }
+        return data
+      })
+    }
+
+    // Return the item from the cache
+    // @ts-ignore
+    return Promise.resolve(this.cache[props[this.hashKey]])
+  }
+
+  deleteByKey(key: string) {
+    delete this.cache[key]
+    return this.cache
+  }
+
+  flush() {
+    const cacheLength = Object.keys(this.cache).length
+
+    this.cache = {}
+
+    return cacheLength
+  }
+}
+
+export default Cache
---
+++ test/in-memory-fetch-cache.test.ts
@@ -0,0 +1,127 @@
+import * as fs from 'fs'
+import path from 'path'
+import Cache from '../src/in-memory-fetch-cache'
+
+import { User } from './user.dto'
+
+const dataSource = {
+  fetchFunction: async ({ id }: { id: number }) => {
+    const data = fs.readFileSync(path.join(__dirname, `/data.json`), 'utf8')
+
+    const item = JSON.parse(data).filter((item: User) => item.id === id)
+
+    return item[0]
+  }
+}
+
+describe('in memory fetch cache', () => {
+  afterEach(() => jest.clearAllMocks())
+
+  it('it should cache and return the data', async () => {
+    const fetchFunctionSpy = jest.spyOn(dataSource, 'fetchFunction')
+
+    const cache = new Cache<User, { id: number }>({
+      fetchFunction: dataSource.fetchFunction,
+      hashKey: 'id'
+    })
+
+    const getDataSpy = jest.spyOn(cache, 'getData')
+
+    const firstFetch = await cache.getData({ id: 1 })
+    expect(firstFetch.id).toBe(1)
+    expect(firstFetch.name).toBe('Leanne Graham')
+    const secondFetch = await cache.getData({ id: 1 })
+
+    expect(secondFetch.id).toBe(1)
+    expect(secondFetch.name).toBe('Leanne Graham')
+
+    expect(getDataSpy).toHaveBeenCalledTimes(2)
+    expect(fetchFunctionSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('it should add all items to the cache', async () => {
+    const fetchFunctionSpy = jest.spyOn(dataSource, 'fetchFunction')
+
+    const cache = new Cache<User, { id: number }>({
+      fetchFunction: dataSource.fetchFunction,
+      hashKey: 'id'
+    })
+
+    const getDataSpy = jest.spyOn(cache, 'getData')
+
+    const firstFetch = await cache.getData({ id: 1 })
+    expect(firstFetch.id).toBe(1)
+    expect(firstFetch.name).toBe('Leanne Graham')
+
+    const secondFetch = await cache.getData({ id: 2 })
+    expect(secondFetch.id).toBe(2)
+    expect(secondFetch.name).toBe('Ervin Howell')
+
+    expect(getDataSpy).toHaveBeenCalledTimes(2)
+    expect(fetchFunctionSpy).toHaveBeenCalledTimes(2)
+  })
+
+  it('it should not cache null values', async () => {
+    const fetchFunctionSpy = jest.spyOn(dataSource, 'fetchFunction')
+
+    const cache = new Cache<User, { id: number }>({
+      fetchFunction: dataSource.fetchFunction,
+      hashKey: 'id'
+    })
+
+    const getDataSpy = jest.spyOn(cache, 'getData')
+
+    const firstFetch = await cache.getData({ id: 10000 })
+    expect(firstFetch).toBeUndefined()
+  })
+
+  it('it should delete item by key', async () => {
+    const fetchFunctionSpy = jest.spyOn(dataSource, 'fetchFunction')
+
+    const cache = new Cache<User, { id: number }>({
+      fetchFunction: dataSource.fetchFunction,
+      hashKey: 'id'
+    })
+
+    const getDataSpy = jest.spyOn(cache, 'getData')
+
+    const firstFetch = await cache.getData({ id: 1 })
+    expect(firstFetch.id).toBe(1)
+    expect(firstFetch.name).toBe('Leanne Graham')
+
+    await cache.deleteByKey('1')
+
+    const secondFetch = await cache.getData({ id: 1 })
+
+    expect(secondFetch.id).toBe(1)
+    expect(secondFetch.name).toBe('Leanne Graham')
+
+    expect(getDataSpy).toHaveBeenCalledTimes(2)
+    expect(fetchFunctionSpy).toHaveBeenCalledTimes(2)
+  })
+
+  it('it should flush the cache', async () => {
+    const fetchFunctionSpy = jest.spyOn(dataSource, 'fetchFunction')
+
+    const cache = new Cache<User, { id: number }>({
+      fetchFunction: dataSource.fetchFunction,
+      hashKey: 'id'
+    })
+
+    const getDataSpy = jest.spyOn(cache, 'getData')
+
+    const firstFetch = await cache.getData({ id: 1 })
+    expect(firstFetch.id).toBe(1)
+    expect(firstFetch.name).toBe('Leanne Graham')
+
+    await cache.flush()
+
+    const secondFetch = await cache.getData({ id: 1 })
+
+    expect(secondFetch.id).toBe(1)
+    expect(secondFetch.name).toBe('Leanne Graham')
+
+    expect(getDataSpy).toHaveBeenCalledTimes(2)
+    expect(fetchFunctionSpy).toHaveBeenCalledTimes(2)
+  })
+})
